1bf458b37a47efa376c7ae3087528ef3e1599c56
Luke 10X <spam@luke10x.dev>
2022-08-22 04:33:17 -0400

    contract test improved

--- weatherapi/src/test/java/dev/luke10x/mlb/homework/weatherapi/adapter/statsapi/WireMockInitializer.java
+++ weatherapi/src/test/java/dev/luke10x/mlb/homework/weatherapi/adapter/statsapi/WireMockInitializer.java
@@ -1,11 +1,14 @@
 package dev.luke10x.mlb.homework.weatherapi.adapter.statsapi;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
 
 public class WireMockInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -21,5 +24,10 @@ public class WireMockInitializer implements ApplicationContextInitializer<Config
                 .of(Map.of("mlbStatsApi.url", statsWireMockServer.baseUrl(),
                 "nwsWeatherApi.url", weatherWireMockServer.baseUrl()))
                 .applyTo(applicationContext);
+    }
+
+    public static WireMock recreateWiremockByUrl(String urlString) throws MalformedURLException {
+        var url = new URL(urlString);
+        return new WireMock(url.getProtocol(), url.getHost(), url.getPort());
     }
 }
